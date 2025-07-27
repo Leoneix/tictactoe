fc1964509271aa7c278383e02e016efeadb20234
Partha Pratim Basak <leoneix820@gmail.com>
2025-07-27 10:55:02 +0530

    game

--- script.js
+++ script.js
@@ -1,5 +1,94 @@
-class name {
-    constructor(parameters) {
-        
+// Create Rock, Paper, Scissors array
+const CHOICES = ["Rock", "Paper", "Scissors"];
+
+let gamesPlayed = 0;
+let gamesWon = 0;
+
+// Start game sequence with user click
+document.addEventListener("click", (e) => {
+  if (e.target.value != null || e.target.value != undefined) {
+    gamesPlayed++;
+    let userSelection = getUserSelection(e.target);
+    let compSelection = getCompSelection();
+
+    updateView("", userSelection, compSelection);
+
+    //  Compare user input to computer input
+    if (userSelection === compSelection) {
+      updateView("It's a tie!");
+    } else {
+      if (userSelection === "Rock") {
+        compSelection === "Paper"
+          ? updateView(compWins())
+          : updateView(userWins());
+      } else if (userSelection === "Paper") {
+        compSelection === "Scissors"
+          ? updateView(compWins())
+          : updateView(userWins());
+      } else if (userSelection === "Scissors") {
+        compSelection === "Rock"
+          ? updateView(compWins())
+          : updateView(userWins());
+      }
     }
+    updateView("", "", "", calcWinPercentage());
+  }
+});
+
+//  Collect and store user input (Rock, Paper, or Scissors)
+function getUserSelection(target) {
+  return target.value;
+}
+
+//  Create computer input
+function getCompSelection() {
+  let i = Math.floor(Math.random() * 3);
+  return CHOICES[i];
+}
+
+//  Alert the winner
+function updateView(message, userSelection, compSelection, winPercent) {
+  if (message) {
+    document.getElementById("gameboard-para").textContent = message;
+  }
+
+  if (userSelection) {
+    document.getElementById("user-selection").textContent = userSelection;
+  }
+
+  if (compSelection) {
+    document.getElementById("computer-selection").textContent = compSelection;
+  }
+
+  if (winPercent) {
+    if (document.getElementById("win-percentage") === null) {
+      let para = document.createElement("p");
+      para.setAttribute("class", "gameboard__win-percentage");
+      para.setAttribute("id", "win-percentage");
+      document.getElementById("gameboard-dialog").appendChild(para);
+    }
+    if (gamesPlayed < 10) {
+      document.getElementById("win-percentage").textContent = `
+        Play ${10 - gamesPlayed} more games to see your win percentage.
+      `;
+    } else {
+      document.getElementById("win-percentage").textContent = `
+        Win Percentage: ${winPercent}
+      `;
+    }
+  }
+}
+
+function userWins() {
+  gamesWon++;
+  return "You win!";
+}
+
+function compWins() {
+  return "You lose...";
+}
+
+function calcWinPercentage() {
+  let decimal = gamesWon / gamesPlayed;
+  return `${(decimal * 100).toFixed(2)}%`;
 }
